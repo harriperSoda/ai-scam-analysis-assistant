a18624063936d9e08348be412d02a92ae8429fe9
Aman <amanharripersadh12@gmail.com>
2026-08-27 21:48:29 +0200

    OpenAI API connection succesful

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import os
 ##imports load_dotnev function from python-dotnev package we installed
 from dotenv import load_dotenv
+from openai import OpenAI
 
 load_dotenv() ## Finds the .env file and loads its variables into the programs env
 ## Looks for env variable named OPENAI_API_KEY and stores its value inside api_key
@@ -10,5 +11,18 @@ api_key = os.getenv("OPENAI_API_KEY")
 
 if api_key:
     print("API key loaded successfully.")
+    client = OpenAI(api_key=api_key)
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages = [
+            {
+            "role": "user",
+            "content":"Reply with exactly: Connection successful."
+            }
+        ],
+        max_completion_tokens = 20
+    )
+    print(response.choices[0].message.content)
 else:
     print("Failed to load API key.")
